a1968e1a62a819599115dc5a67388948d8a0ebc2
user <user@MacBook-Pro-user.local>
2025-07-23 20:53:27 +0300

    WIP Drivers

--- src/components/sections/drivers.tsx
+++ src/components/sections/drivers.tsx
@@ -1,7 +1,53 @@
+import ExportedImage from 'next-image-export-optimizer'
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+
 export default function Drivers() {
   return (
     <>
-      <h2>Meet the Drivers</h2>
+      <h2>Знайомтесь з водіями</h2>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <DriverCard name="водій X" />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <DriverCard name="водій Y" />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <DriverCard name="водій Z" />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </>
+  )
+}
+function DriverCard({
+  img = '/images/driver.jpeg',
+  name = 'водій Х',
+  kart = 'Sodi GT-series',
+}: {
+  img?: string
+  name?: string
+  kart?: string
+}) {
+  return (
+    <div className="flex flex-col items-center">
+      <ExportedImage src={img} alt="Large Image" width={320} height={240} sizes="100vw" />
+      <p>
+        Водій: <b>{name}</b>
+      </p>
+      <p>
+        Карт: <b>{kart}</b>
+      </p>
+    </div>
   )
 }
