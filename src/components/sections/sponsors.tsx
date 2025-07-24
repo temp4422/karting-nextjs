1efa137ed66e121cac7f0d39148835331f58e159
user <user@MacBook-Pro-user.local>
2025-07-24 14:55:15 +0300

    WIP Sponsors

--- src/components/sections/sponsors.tsx
+++ src/components/sections/sponsors.tsx
@@ -1,9 +1,22 @@
 export default function Sponsors() {
   return (
     <>
-      <h2>Sponsors & Partners</h2>
+      <h2>Спонсори та Партнери</h2>
+
+      <p>
+        Мережа прокатів гірськолижного спорядждення{' '}
+        <a className="text-primary underline" href="https://x-drive.ua" target="_blank">
+          x-drive.ua
+        </a>
+      </p>
+
+      <p>
+        Якщо ви хочете стати спонсором або партнером нашого клубу, будь ласка, зв'яжіться з нами
+        через{' '}
+        <a className="text-primary underline" href="https://t.me/" target="_blank">
+          Telegram
+        </a>
+      </p>
     </>
   )
 }
-
-
