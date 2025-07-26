3d223115f54e260ba81b942458074dc24a77f708
user <user@MacBook-Pro-user.local>
2025-07-26 14:55:28 +0300

    bun add -D prettier prettier-plugin-tailwindcss

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   images: {
     loader: 'custom',
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    deviceSizes: [640, 750, 828, 1080, 1200],
   },
   transpilePackages: ['next-image-export-optimizer'],
   env: {
